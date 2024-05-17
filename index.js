03aee09555c69cdcbadac08e47ce6974e8c637f3
Jaswanth Sudha <119607624+JaswanthSudha@users.noreply.github.com>
2024-05-17 10:38:59 +0530

    commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config()
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require("express")
+const app = express()
+app.use(express.json())
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+// ...
+
+
+app.get("/home", (req, res) => {
+    res.json("HomePage")
+})
+app.post("/data", async (req, res) => {
+    const { input } = req.body
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    // const prompt = "Write a story about a magic backpack."
+
+    const result = await model.generateContent(input);
+    const response = await result.response;
+    const text = response.text();
+    res.json({ "message": text })
+})
+app.listen(4000, () => {
+    console.log("listening to the port")
+})
+
+
+// run("what is the capital of telangana?");
+
+// ...
